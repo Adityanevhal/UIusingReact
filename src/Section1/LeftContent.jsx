462770c76ce7bcdae27a0a527bbc6ae00e41f73c
Aditya Nevhal <adityanevhal1@gmail.com>
2026-03-05 02:15:13 +0530

    up and icon

---
+++ src/Section1/LeftContent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {SquareArrowOutUpRight} from 'Lucide-react'
+const LeftContent = () => {
+  return (
+    <div className='h-full w-1/4 bg-amber-200'>
+
+<div></div>
+<div>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos, accusantium.</div>
+<div> <SquareArrowOutUpRight /></div>
+
+    </div>
+  )
+}
+
+export default LeftContent
---
+++ src/Section1/RightContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const RightContent = () => {
+  return (
+    <div className='h-full w-3/4 bg-yellow-400'>
+        </div>
+  )
+}
+
+export default RightContent
